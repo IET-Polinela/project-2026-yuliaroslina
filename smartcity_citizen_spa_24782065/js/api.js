b1c4810ad4e90e37d6881760bacb3875f28a42d9
yuliaroslina <yuliarosln@gmail.com>
2026-06-10 16:09:40 +0700

    Lab Session 13 - deploy SPA to GitHub Pages

--- smartcity_citizen_spa_24782065/js/api.js
+++ smartcity_citizen_spa_24782065/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "http://103.151.63.87:8002";
 
 async function requestAPI(endpoint, method = "GET", bodyData = null) {
     const accessToken = localStorage.getItem("access_token");
